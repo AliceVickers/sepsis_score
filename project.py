0f94f3b96ff018eeb621441cfe816379938b25d1
eh14274 <ENHardy@Emmas-MacBook-Air-2.local>
2017-10-10 20:01:21 +0100

    Condensded the first section of code so it is neater, added and/if statements too

--- project.py
+++ project.py
@@ -1,25 +1,17 @@
-print "What is the respiratory rate (breaths per minute)?"
-respiratory_rate = int(raw_input("> "))
+systolic_pressure = int(raw_input("What is the systolic blood pressure (mmHg)? "))
 
-print "What is the systolic blood pressure (mmHg)?"
-systolic_pressure = int(raw_input("> "))
+heart_rate = int(raw_input("What is the heart rate (bpm)? "))
 
-print "What is the heart rate (bpm)?"
-heart_rate = int(raw_input("> "))
+respiratory_rate = int(raw_input("What is the respiratory rate? "))
 
-print "What is the body temeprature (Celsius)"
-body_temperature = int(raw_input("> "))
+urine_output = int(raw_input("What is the 4 hour urine output? "))
 
-print "What is the 4 hour urine output?"
-urine_output = int(raw_input("> "))
-
-print "State the patient response level:\n"
-print """Alert = 0
-Voice = 1
-Agitation/Confusion = 2
-Pain = 3
-Unresponsive = 4\n """
-response_level = raw_input("> ")
+consciousness = int(raw_input("""State the level of consciousness: 
+1 - Unresponsive 
+2 - Pain
+3 - Agitation/Confusion 
+4 - Voice 
+5 - Alert """))
 
 print """\nPATIENT REPORT:
 This patient has a respiratory rate of %r breaths per minute,
@@ -59,3 +51,16 @@ elif respiratory_rate >5 and respiratory_rate >8 and systolic_pressure >70 and s
 	print "Sepsis warning level is orange."
 elif respiratory_rate >5 and respiratory_rate >8 and systolic_pressure >80 and systolic_pressure <89 and heart_rate >40 and heart_rate <49 and urine_output >80 and urine_output <120 and response_level == "2":
 	print "Spesis warning level is yellow."
+
+
+
+if systolic_pressure in range (100, 180) and heart_rate in range (50, 100) and respiratory_rate in range (9, 20):
+	print "Pateint in WHITE category"
+if (systolic_pressure in range (90, 99) and consciousness == 1) or (heart_rate in range (101, 110)):
+	print "Pateint in YELLOW category"
+#if systolic_pressure in range (80, 89) and urine_output in range (80, 120) and consciousness == 2 or (respiratory_rate in range (21, 30) and systolic_pressure > 180 and heart_rate in range (111, 130)):
+#	print "Pateint in GOLD category"
+if (systolic_pressure <  70 and heart_rate < 40 and respiratory_rate < 5) or (respiratory_rate > 35 and heart_rate > 140):
+	print "Pateint in PINK category"
+
+
