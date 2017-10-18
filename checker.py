89136634bc4e4cd4742910d63e5421d6c2827b6e
Alice <alice@Annies-MacBook-Pro.local>
2017-10-18 20:14:18 +0100

    new changes

--- checker.py
+++ checker.py
@@ -7,7 +7,5 @@ app = Flask("MyForm")
 @app.route("/symptomchecker")
 def hello_someone():
     return render_template("project.html")
-    print form_data["name"]
-    print form_data["admission"]
 
 app.run(debug=True)
