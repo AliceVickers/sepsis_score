dc7c837eac3922e0fe17f3c3144fd109a27fc025
Alice <alice@Annies-MacBook-Pro.local>
2017-10-18 19:52:00 +0100

    "new changes"

--- project.py
+++ project.py
@@ -13,9 +13,7 @@ consciousness = int(raw_input("""State the level of consciousness:
 2 - Pain
 3 - Agitation/Confusion
 4 - Voice
-5 - Alert
-6 - Hello
-7 - Change """))
+5 - Alert """))
 
 print """\nPATIENT REPORT:
 This patient has a respiratory rate of %r breaths per minute,
